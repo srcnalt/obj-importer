c01245a5e6efbc78e3e7be8b84148970d81f604b
Sarge <sercan.altundas@gmail.com>
2018-07-27 16:31:34 +0300

    [feature] can import single mesh uv-less models

--- ObjImporter/Importer.cpp
+++ ObjImporter/Importer.cpp
@@ -1,77 +1,199 @@
 #include "Importer.h"
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+struct Point3
+{
+	float x, y, z = 0;
+
+	inline Point3() {};
+
+	inline Point3(float x, float y, float z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+};
+
+struct Point2
+{
+	float x, y = 0;
+
+	inline Point2() {};
+
+	inline Point2(const float x, const float y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
+struct Sizes {
+	int vSize;
+	int nSize;
+	int fSize;
+	int uSize;
+
+	inline Sizes() {
+		vSize = 0;
+		nSize = 0;
+		fSize = 0;
+		uSize = 0;
+	}
+};
+
+string thePath;
+string objName;
+string matName;
+string texName;
+vector<Point3> vertices;
+vector<Point3> normals;
+vector<Point2> uvs;
+vector<int> triangles;
+vector<int> faceIndexes;
+vector<Point3> faces;
+
+Sizes *sizes = new Sizes();
+
+bool validateLine(string str) {
+	return	str.find("f ") == 0 || str.find("v ") == 0 || str.find("vt ") == 0 || str.find("vn ") == 0 || str.find("mtllib ") == 0;
+}
+
+vector<string> split(string str, char delimineter) {
+	vector<string> pieces;
+	stringstream stream(str);
+	string token;
+
+	while (getline(stream, token, delimineter)) {
+		pieces.push_back(token);
+	}
+
+	return pieces;
+}
+
+void CreateMesh(string path)
+{
+	char space = ' ';
+	char slash = '/';
+
+	int f = 0;
+	vector<Point3> vx;
+	vector<Point2> uv;
+	vector<Point3> nr;
+	
+	thePath = path.substr(0, path.find_last_of("\\/"));
+	objName = path.substr(path.find_last_of("\\/") + 1);
+
+	string line;
+	ifstream file(path);
+
+	while (getline(file, line)) {
+		
+		if (validateLine(line)) {
+			vector<string> pieces = split(line, space);
+
+			if (pieces[0] == "mtllib") {
+				matName = pieces[1];
+				continue;
+			}
+			
+			if (pieces[0] == "v") {
+				vx.push_back(Point3(stof(pieces[1]), stof(pieces[2]), stof(pieces[3])));
+				continue;
+			}
+
+			if (pieces[0] == "vt") {
+				uv.push_back(Point2(stof(pieces[1]), stof(pieces[2])));
+				continue;
+			}
+
+			if (pieces[0] == "vn") {
+				nr.push_back(Point3(stof(pieces[1]), stof(pieces[2]), stof(pieces[3])));
+				continue;
+			}
+
+			if (pieces[0] == "f") {
+				int j = 1;
+				faceIndexes.clear();
+				while (j < pieces.size() && 0 < pieces[j].size())
+				{
+					Point3 temp;
+					vector<string> facePieces = split(pieces[j], slash);
+					temp.x = stoi(facePieces[0]);
+
+					if (facePieces.size() > 1)
+					{
+						temp.y = facePieces[1] != "" ? stoi(facePieces[1]) : 0;
+						temp.z = stoi(facePieces[2]);
+					}
+					faces.push_back(temp);
+					faceIndexes.push_back(f);
+
+					j++;
+					f++;
+				}
+
+				j = 1;
+
+				while (j + 2 < pieces.size()) {
+					triangles.push_back(faceIndexes[0]);
+					triangles.push_back(faceIndexes[j]);
+					triangles.push_back(faceIndexes[j + 1]);
+					j++;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < faces.size(); i++)
+	{
+		vertices.push_back(vx[(int)faces[i].x - 1]);
+
+		if (faces[i].y >= 1) uvs.push_back(uv[(int)faces[i].y - 1]);
+		if (faces[i].z >= 1) normals.push_back(nr[(int)faces[i].z - 1]);
+	}
+}
+
 extern "C" {
-	struct Vector3
+	void ImportFile(const char *path)
 	{
-		float x, y, z;
+		string filePath(path);
 
-		inline Vector3(void) {}
+		CreateMesh(filePath);
 
-		inline Vector3(const float x, const float y, const float z)
-		{
-			this->x = x;
-			this->y = y;
-			this->y = z;
-		}
+		//return mesh;
+	}
 
-		//sum with another Vector3
-		inline Vector3 operator + (const Vector3& V) const
-		{
-			return Vector3(x + V.x, y + V.y, z + V.z);
-		}
+	Sizes* GetSizes() {
+		sizes->vSize = vertices.size();
+		sizes->nSize = normals.size();
+		sizes->fSize = faces.size();
+		sizes->uSize = uvs.size();
 
-		//sum with float
-		inline Vector3 operator + (const float V) const
-		{
-			return Vector3(x + V, y + V, z + V);
-		}
-	};
+		return sizes;
+	}
 
-	struct Vector2
-	{
-		float x, y;
+	Point3* GetVertices() {
+		return &vertices[0];
+	}
 
-		inline Vector2(void) {}
+	Point3* GetNormals() {
+		return &normals[0];
+	}
 
-		inline Vector2(const float x, const float y)
-		{
-			this->x = x;
-			this->y = y;
-		}
+	Point2* GetUVs() {
+		return &uvs[0];
+	}
 
-		//sum with another Vector2
-		inline Vector2 operator + (const Vector2& V) const
-		{
-			return Vector2(x + V.x, y + V.y);
-		}
-
-		//sum with float
-		inline Vector2 operator + (const float V) const
-		{
-			return Vector2(x + V, y + V);
-		}
-	};
-
-	struct MeshStruct
-	{
-		string name;
-		string fileName;
-		vector<Vector3> vertices;
-		vector<Vector3>normals;
-		vector<Vector3> faces;
-		vector<Vector2> uv;
-		vector<Vector2> uv1;
-		vector<Vector2> uv2;
-		vector<int> triangles;
-		vector<int> faceVerts;
-		vector<int> faceUVs;
-	};
-
-	int ImportObj(string path) {
-
+	int* GetFaces() {
+		return &triangles[0];
 	}
 }
--- ObjImporter/Importer.h
+++ ObjImporter/Importer.h
@@ -5,5 +5,15 @@ using namespace std;
 #define DLL_API __declspec(dllexport) 
 
 extern "C" {
-	DLL_API int ImportObj(string path);
+	struct Sizes;
+	struct Point3;
+	struct Point2;
+	DLL_API Sizes* GetSizes();
+
+	DLL_API Point3* GetVertices();
+	DLL_API Point3* GetNormals();
+	DLL_API Point2* GetUVs();
+	DLL_API int* GetFaces();
+
+	DLL_API void ImportFile(const char *path);
 }
